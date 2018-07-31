5a6a3b63d9cb1164b63156b8b40a37cc4e345257
Renaud Paquay <rpaquay@hotmail.com>
2018-07-30 17:52:52 -0700

    Increase version #

--- src/shared/VersionNumber.cs
+++ src/shared/VersionNumber.cs
@@ -13,7 +13,7 @@
 // limitations under the License.
 namespace mtsuite.shared {
   public static class VersionNumber {
-    public const string Product = "0.9.6";
+    public const string Product = "0.9.7";
     public const string File = Product + ".0";
   }
 }
