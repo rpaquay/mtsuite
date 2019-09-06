f51f40eea317bd86299d01fb5546937f5a44e2a8
Renaud Paquay <rpaquay@hotmail.com>
2019-09-06 10:08:54 -0700

    Create "MtGrep" project as simple copy of MtFind

---
+++ src/mtgrep/MtGrep.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2015 Renaud Paquay All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+using mtsuite.CoreFileSystem;
+using mtsuite.shared;
+using mtsuite.shared.CommandLine;
+using mtsuite.shared.Utils;
+
+namespace mtgrep {
+  public class MtGrep {
+    private readonly IFileSystem _fileSystem;
+    private readonly ParallelFileSystem _parallelFileSystem;
+    private readonly MtGrepProgressMonitor _progressMonitor;
+
+    public MtGrep(IFileSystem fileSystem) {
+      _fileSystem = fileSystem;
+      _parallelFileSystem = new ParallelFileSystem(fileSystem);
+      _progressMonitor = new MtGrepProgressMonitor();
+
+      _parallelFileSystem.Error += exception => _progressMonitor.OnError(exception);
+      _parallelFileSystem.Pulse += () => _progressMonitor.Pulse();
+
+      _parallelFileSystem.EntriesDiscovered += (entry, list) => _progressMonitor.OnEntriesDiscovered(entry, list);
+      _parallelFileSystem.DirectoryTraversing += (entry) => _progressMonitor.OnDirectoryTraversing(entry);
+      _parallelFileSystem.DirectoryTraversed += (entry) => _progressMonitor.OnDirectoryTraversed(entry);
+    }
+
+    public void Run(string[] args) {
+      var arguments = new MtGrepArguments(args);
+      if (!arguments.IsValid || arguments.Values.Help) {
+        DisplayBanner();
+        if (!arguments.Values.Help) {
+          arguments.DisplayArgumentErrors();
+        }
+        arguments.DisplayUsage();
+        throw new CommandLineReturnValueException(16); // To match robocopy
+      }
+
+      var sourcePath = ProgramHelpers.MakeFullPath(arguments.Values.Directory);
+      var pattern = arguments.Values.Pattern;
+      ProgramHelpers.SetWorkerThreadCount(arguments.Values.ThreadCount);
+      bool followLinks = !arguments.Values.NoFollowLinks;
+      bool isPlainOutput = arguments.Values.PlainOutput;
+      if (!isPlainOutput) {
+        DisplayBanner();
+      }
+
+      var matchedFiles = DoFind(sourcePath, pattern, isPlainOutput, arguments.Values.NoProgress, followLinks);
+
+      DisplayMatchesFiles(matchedFiles, pattern, isPlainOutput);
+
+      var statistics = _progressMonitor.GetStatistics();
+      if (!isPlainOutput) {
+        DisplayStatistics(statistics);
+        Console.WriteLine();
+      }
+
+      if (arguments.Values.GarbageCollect) {
+        ProgramHelpers.DisplayGcStatistics();
+      }
+
+      // 0 = success, 8 = fail (to match robocopy)
+      if (statistics.Errors.Count > 0) {
+        throw new CommandLineReturnValueException(8);
+      }
+    }
+
+    private static void DisplayBanner() {
+      Console.WriteLine();
+      Console.WriteLine("-------------------------------------------------------------------------------");
+      Console.WriteLine("MTFIND :: Multi-Threaded File Search for Windows - version {0}",
+        Assembly.GetExecutingAssembly().GetName().Version);
+      Console.WriteLine("-------------------------------------------------------------------------------");
+      Console.WriteLine();
+    }
+
+    public List<FileSystemEntry> DoFind(FullPath sourcePath, string pattern, bool isPlainOutput, bool noProgressOutput, bool followLinks) {
+      _progressMonitor.QuietMode = isPlainOutput || noProgressOutput;
+
+      // Check source exists
+      FileSystemEntry sourceDirectory;
+      try {
+        sourceDirectory = _fileSystem.GetEntry(sourcePath);
+      } catch (Exception e) {
+        Console.WriteLine(e.Message);
+        // 8 = fail (to match robocopy)
+        throw new CommandLineReturnValueException(8);
+      }
+
+      if (!isPlainOutput) {
+        Console.WriteLine("Search file names from \"{0}\"", PathHelpers.StripLongPathPrefix(sourcePath.FullName));
+        Console.WriteLine();
+      }
+      _progressMonitor.Start();
+      var directorySummaryCollector = new MtGrepSummaryCollector(CreateFileNameMatcher(pattern));
+      var task = _parallelFileSystem.TraverseDirectoryAsync(sourceDirectory, directorySummaryCollector, followLinks);
+      _parallelFileSystem.WaitForTask(task);
+      _progressMonitor.Stop();
+      return directorySummaryCollector.MatchedFiles;
+    }
+
+    private static FileNameMatcher CreateFileNameMatcher(string pattern) {
+      var matcher = new SearchPatternParser().ParsePattern(pattern, SearchPatternParser.Options.Optimize);
+      return entry => matcher.MatchString(entry.Path.Name);
+    }
+
+    private static void DisplayStatistics(Statistics statistics) {
+      var elapsedTimeText = FormatHelpers.FormatElapsedTime(statistics.ElapsedTime);
+      var cpuTimeText = FormatHelpers.FormatElapsedTime(statistics.TotalProcessorTime);
+      var directoriesText = string.Format("{0:n0}", statistics.DirectoryTraversedCount);
+      var filesText = string.Format("{0:n0}", statistics.FileEnumeratedCount);
+      var symlinksText = string.Format("{0:n0}", statistics.SymlinkEnumeratedCount);
+      var entriesPerSecondText = string.Format("{0:n0}", statistics.EntryEnumeratedCount / statistics.ElapsedTime.TotalSeconds);
+      var errorsText = string.Format("{0:n0}", statistics.Errors.Count);
+      var fields = new[] {
+        new PrinterEntry("Statistics"),
+        new PrinterEntry("Elapsed time", elapsedTimeText, valueAlign: Align.Right, indent: 2),
+        new PrinterEntry("CPU time", cpuTimeText, valueAlign:Align.Right, indent: 2),
+        new PrinterEntry("# of directories", directoriesText, shortName: "directories", valueAlign: Align.Right, indent: 2),
+        new PrinterEntry("# of files", filesText, shortName: "files", valueAlign: Align.Right, indent: 2),
+        new PrinterEntry("# of symlinks", symlinksText, shortName: "symlinks", valueAlign: Align.Right, indent: 2),
+        new PrinterEntry("# of entries/sec", entriesPerSecondText, shortName:"entries/sec", valueAlign: Align.Right, indent: 2),
+        new PrinterEntry("# of errors", errorsText, shortName:"errors", valueAlign: Align.Right, indent: 2),
+      };
+      Console.WriteLine();
+      FieldsPrinter.WriteLine(fields);
+
+      ProgramHelpers.DisplayErrors(statistics.Errors);
+    }
+
+    private static void DisplayMatchesFiles(List<FileSystemEntry> matchedFiles, string searchPattern, bool isPlainOutput) {
+      var matchedEntries = matchedFiles
+        .OrderBy(entry => entry.Path)
+        .ToList();
+
+      foreach (var entry in matchedEntries) {
+        Console.WriteLine(PathHelpers.StripLongPathPrefix(entry.Path.FullName));
+      }
+      if (!isPlainOutput) {
+        Console.WriteLine("Found {0} entries matching pattern \"{1}\"", matchedEntries.Count, searchPattern);
+      }
+    }
+  }
+}
---
+++ src/mtgrep/MtGrepProgressMonitor.cs
@@ -0,0 +1,74 @@
+// Copyright 2015 Renaud Paquay All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.ComponentModel;
+using mtsuite.CoreFileSystem.Win32;
+using mtsuite.shared;
+using mtsuite.shared.Utils;
+
+namespace mtgrep {
+  public class MtGrepProgressMonitor : ProgressMonitor {
+    public bool QuietMode { get; set; }
+
+    protected override void DisplayStatus(Statistics statistics) {
+      if (QuietMode) {
+        return;
+      }
+
+      var elapsedTimeText = string.Format("{0}", FormatHelpers.FormatElapsedTime(statistics.ElapsedTime));
+      var cpuTimeText = string.Format("{0}", FormatHelpers.FormatElapsedTime(statistics.TotalProcessorTime));
+      var directoriesText = string.Format("{0:n0}", statistics.DirectoryTraversedCount);
+      var filesText = string.Format("{0:n0}", statistics.EntryEnumeratedCount);
+      var entriesPerSecondText = string.Format("{0:n0}", statistics.EntryEnumeratedCount / statistics.ElapsedTime.TotalSeconds);
+      var errorsText = string.Format("{0:n0}", statistics.Errors.Count);
+
+      var fields = new[] {
+        new PrinterEntry("Elapsed time", elapsedTimeText, valueAlign: Align.Right),
+        new PrinterEntry("CPU time", cpuTimeText, valueAlign:Align.Right),
+        new PrinterEntry("# of directories", directoriesText, shortName: "directories", valueAlign: Align.Right),
+        new PrinterEntry("# of files", filesText, shortName: "files", valueAlign: Align.Right),
+        new PrinterEntry("# of files/sec", entriesPerSecondText, shortName:"files/sec", valueAlign: Align.Right),
+        new PrinterEntry("# of errors", errorsText, shortName:"errors", valueAlign: Align.Right),
+      };
+      Print(fields);
+    }
+
+    /// <summary>
+    /// Ignore errors that are harmless, such as inability to enumerate files in
+    /// a directory.
+    /// </summary>
+    public override void OnError(Exception e) {
+      if (IsIgnorableError(e)) {
+        return;
+      }
+
+      base.OnError(e);
+    }
+
+    private bool IsIgnorableError(Exception e) {
+      var win32Error = e as Win32Exception;
+      if (win32Error == null) {
+        return false;
+      }
+
+      switch (win32Error.NativeErrorCode) {
+        case (int)Win32Errors.ERROR_PATH_NOT_FOUND:
+          return true;
+        default:
+          return false;
+      }
+    }
+  }
+}
