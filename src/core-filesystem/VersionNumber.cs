365dcadd6f67ef144b174760986ce3f56bc8d842
Renaud Paquay <rpaquay@google.com>
2026-08-07 20:07:33 -0700

    Bump version number to 0.9.22

--- src/core-filesystem/VersionNumber.cs
+++ src/core-filesystem/VersionNumber.cs
@@ -1,4 +1,4 @@
-// Copyright 2026 Renaud Paquay All Rights Reserved.
+﻿// Copyright 2026 Renaud Paquay All Rights Reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 // limitations under the License.
 namespace mtsuite.CoreFileSystem {
   public static class VersionNumber {
-    public const string Product = "0.9.21";
+    public const string Product = "0.9.22";
     public const string File = Product + ".0";
   }
 }
