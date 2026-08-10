9f825d4243afaf598a09fd4b192f686b54ff4346
Renaud Paquay <rpaquay@google.com>
2026-08-09 17:26:05 -0700

    Code cleanup, error handling

--- src/shared/ParallelFileSystem.cs
+++ src/shared/ParallelFileSystem.cs
@@ -38,8 +38,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
   /// at every invocation.
   /// </summary>
   private readonly CopyFileCallback<CopyFileData> _copyFileCallback =
-    static (ref FileSystemEntry sourceEntry, long bytesFromPreviousCall, long bytesSoFar, long totalBytes,
-      ref CopyFileData data) => {
+    static (ref FileSystemEntry sourceEntry, long bytesFromPreviousCall, long bytesSoFar, long _, ref CopyFileData data) => {
       data.Instance.OnFileCopyingProgress(sourceEntry, data.Stopwatch.Elapsed, bytesFromPreviousCall, bytesSoFar);
     };
 
@@ -48,8 +47,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
   /// at every invocation.
   /// </summary>
   private readonly CompareFileCallback<CompareFileData> _compareFileCallback =
-    static (ref FileSystemEntry sourceEntry, long bytesFromPreviousCall, long bytesSoFar, long totalBytes,
-      ref CompareFileData data) => {
+    static (ref FileSystemEntry sourceEntry, long bytesFromPreviousCall, long bytesSoFar, long _, ref CompareFileData data) => {
       data.Instance.OnFileComparingProgress(sourceEntry, data.Stopwatch.Elapsed, bytesFromPreviousCall, bytesSoFar);
     };
 
@@ -271,7 +269,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
       return;
     }
     var destinationDirectory = optionalDestinationDirectory.Value;
-    var isCloningActive = IsCloningSupported(sourceDirectory, destinationDirectory, options, useCloning);
+    var isCloningActive = ShouldUseCloneFile(sourceDirectory, destinationDirectory, options, useCloning);
 
     Task finalTask;
     {
@@ -326,16 +324,16 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
       //
       {
         using var taskList = _taskListPool.AllocateFrom();
-        foreach (var entry in sourceEntries.Item) {
-          if (entry.IsFile || entry.IsReparsePoint) {
-            PerformOrScheduleFileEntryCopy(entry, destinationDirectory, fileComparer, destinationSet.Item,
+        foreach (var sourceEntry in sourceEntries.Item) {
+          if (sourceEntry.IsRegularFile || sourceEntry.IsReparsePoint) {
+            PerformOrScheduleFileEntryCopy(sourceEntry, destinationDirectory, fileComparer, destinationSet.Item,
               taskList.Item, isCloningActive);
           }
-          else if (entry.IsDirectory) {
-            var destinationEntryPath = new FullPath(destinationDirectory.Path, entry.Name);
-            var destinationExists = destinationSet.Item.TryGet(entry, out var childDestEntry);
+          else if (sourceEntry.IsRegularDirectory) {
+            var destinationEntryPath = new FullPath(destinationDirectory.Path, sourceEntry.Name);
+            var destinationExists = destinationSet.Item.TryGet(sourceEntry, out var childDestEntry);
             taskList.Item.Add(CopyDirectoryAsync(
-              entry,
+              sourceEntry,
               destinationEntryPath,
               destinationExists ? childDestEntry : null,
               options,
@@ -395,7 +393,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
       foreach (var sourceEntry in sourceEntries.Item) {
         if (destinationSet.Item.TryGet(sourceEntry, out var destinationEntry)) {
           if (sourceEntry.IsRegularFile && destinationEntry.IsRegularFile) {
-            PerformOrScheduleFileEntryClone(sourceEntry, destinationEntry, fileComparer, dryRun, taskList.Item);
+            PerformOrScheduleCloneFileIfNeeded(sourceEntry, destinationEntry, fileComparer, dryRun, taskList.Item);
           }
           else if (sourceEntry.IsRegularDirectory && destinationEntry.IsRegularDirectory) {
             taskList.Item.Add(CompactDirectoryAsync(sourceEntry, destinationEntry, fileComparer, dryRun));
@@ -412,6 +410,9 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     }
   }
 
+  /// <summary>
+  /// Delete all entries of <paramref name="sourceDirectory"/> recursively, including <paramref name="sourceDirectory"/> iself
+  /// </summary>
   private async Task<bool> DeleteDirectoryAsync(FileSystemEntry sourceDirectory, Func<FileSystemEntry, bool> includeFilter) {
     // DeleteDirectoryEntriesAsync does a lot of synchronous I/O, so we run it in a dedicated task/thread.
     return await Task.Run(async () => {
@@ -424,7 +425,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
   }
 
   /// <summary>
-  /// Delete all entries of <paramref name="sourceDirectory"/>, but not <paramref name="sourceDirectory"/> iself
+  /// Delete all entries of <paramref name="sourceDirectory"/> recursively, but not <paramref name="sourceDirectory"/> iself
   /// </summary>
   private async Task<bool> DeleteDirectoryEntriesAsync(FileSystemEntry sourceDirectory, Func<FileSystemEntry, bool> includeFilter) {
     //
@@ -444,7 +445,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     var allEntriesDeleted = true;
     using (var deleteSubDirTaskList = _boolTaskListPool.AllocateFrom()) {
       foreach (var entry in sourceEntries.Item) {
-        if (entry.IsFile || entry.IsRegularFile) {
+        if (entry.IsFile || entry.IsReparsePoint) {
           if (!DeleteSingleEntry(entry, includeFilter)) {
             allEntriesDeleted = false;
           }
@@ -465,7 +466,15 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     return allEntriesDeleted;
   }
   
-  private bool IsCloningSupported(FileSystemEntry sourceDirectory, FileSystemEntry destinationDirectory,
+  /// <summary>
+  /// Returns whether the current operation should use <see cref="PerformCloneFile"/>
+  /// </summary>
+  /// <param name="sourceDirectory"></param>
+  /// <param name="destinationDirectory"></param>
+  /// <param name="options"></param>
+  /// <param name="useCloning"></param>
+  /// <returns></returns>
+  private bool ShouldUseCloneFile(FileSystemEntry sourceDirectory, FileSystemEntry destinationDirectory,
     CopyOptions options, bool? useCloning) {
     return useCloning ?? ((options & CopyOptions.NoClone) == 0 &&
                           _fileSystem.Extension.IsCloningSupported(sourceDirectory.Path,
@@ -542,18 +551,22 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     return entriesToDelete;
   }
 
+  /// <summary>
+  /// Copy (or clone) a regular file or symlink from <paramref name="sourceEntry"/> to
+  /// <paramref name="destinationDirectory"/> unless the file already exist and is the same as the source file
+  /// (see <paramref name="fileComparer"/>
+  ///
+  /// Note: Errors are reported via <see cref="OnError"/> 
+  /// </summary>
   private void PerformOrScheduleFileEntryCopy(
     FileSystemEntry sourceEntry,
     FileSystemEntry destinationDirectory,
     IFileComparer fileComparer,
     SmallSet<FileSystemEntry> destinationDirectoryEntries,
     List<Task> fileTaskList,
-    bool useCloning) {
-
-    // This method only copies regular files and reparse points
-    if (!sourceEntry.IsFile && !sourceEntry.IsReparsePoint) {
-      return;
-    }
+    bool useCloneFile) {
+    Guard.CheckArgument(sourceEntry.IsFile || sourceEntry.IsReparsePoint,
+      "Internal error: Only regular files and reparse points can be copied directly");
 
     var destinationExists = destinationDirectoryEntries.TryGet(sourceEntry, out var destinationEntry);
     var destinationPath = destinationExists
@@ -562,28 +575,30 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
 
     if (destinationExists) {
       try {
-        var areEqual = CompareFiles(fileComparer, sourceEntry, destinationEntry);
+        var areEqual = CompareFiles(sourceEntry, destinationEntry, fileComparer);
         if (areEqual) {
           OnFileCopySkipped(sourceEntry);
           return;
         }
       } catch (Exception e) {
-        // If we can't compare files, log error and continue with normal copy operation.
+        // If we can't compare files, log error and bail out
         OnError(sourceEntry.Path, e);
+        return;
       }
     }
 
-    if (useCloning && sourceEntry.IsFile && !sourceEntry.IsReparsePoint) {
+    // We can only clone regular files (not directory and not symlinks)
+    if (useCloneFile && sourceEntry.IsRegularFile) {
       // If file size is >= threshold, offload cloning to a background task
       if (sourceEntry.FileSize >= _largeFileAsyncThreshold) {
-        fileTaskList.Add(Task.Run(() => PerformCloneFile(sourceEntry, destinationPath)));
+        fileTaskList.Add(Task.Run(() => PerformCloneFile(sourceEntry, destinationPath, dryRun: false)));
       } else {
-        PerformCloneFile(sourceEntry, destinationPath);
+        PerformCloneFile(sourceEntry, destinationPath, dryRun: false);
       }
     } else {
       var copyFileOptions = CopyFileOptions.Default | CopyFileOptions.NoClone;
       // If file size is >= threshold, offload copying to a background task
-      if (sourceEntry.IsFile && sourceEntry.FileSize >= _largeFileAsyncThreshold) {
+      if (sourceEntry.IsRegularFile && sourceEntry.FileSize >= _largeFileAsyncThreshold) {
         fileTaskList.Add(Task.Run(() => PerformCopyFile(sourceEntry, destinationPath, destinationEntry, destinationExists, copyFileOptions)));
       } else {
         PerformCopyFile(sourceEntry, destinationPath, destinationEntry, destinationExists, copyFileOptions);
@@ -591,25 +606,19 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     }
   }
 
-  private void PerformCloneFile(
-    FileSystemEntry sourceEntry,
-    FullPath destinationPath) {
-    var sw = _stopwatchFactory.Create();
-    OnFileCloning(sourceEntry);
-    try {
-      _fileSystem.Extension.CloneFile(sourceEntry, destinationPath);
-    } catch (Exception e) {
-      OnError(sourceEntry.Path, e);
-    }
-    OnFileCloned(sourceEntry, sw.Elapsed, sourceEntry.FileSize);
-  }
-
+  /// <summary>
+  /// Copy the contents of <paramref name="sourceEntry"/> to <paramref name="destinationPath"/>.
+  ///
+  /// Note: Errors are reported via <see cref="OnError"/> 
+  /// </summary>
   private void PerformCopyFile(
     FileSystemEntry sourceEntry,
     FullPath destinationPath,
     FileSystemEntry? destinationEntry,
     bool destinationExists,
     CopyFileOptions copyFileOptions) {
+    Guard.CheckArgument(sourceEntry.IsFile || sourceEntry.IsReparsePoint,
+      "Internal error: Only regular files and reparse points can be copied directly");
     var sw = _stopwatchFactory.Create();
     OnFileCopying(sourceEntry);
     try {
@@ -621,27 +630,59 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
       }
     } catch (Exception e) {
       OnError(sourceEntry.Path, e);
+      return;
     }
     OnFileCopied(sourceEntry, sw.Elapsed, sourceEntry.FileSize);
   }
   
-  private void PerformOrScheduleFileEntryClone(
+  /// <summary>
+  /// Create a clone of <paramref name="sourceEntry"/> in <paramref name="destinationPath"/>.
+  ///
+  /// Note: Errors are reported via <see cref="OnError"/> 
+  /// </summary>
+  private void PerformCloneFile(FileSystemEntry sourceEntry, FullPath destinationPath, bool dryRun) {
+    Guard.CheckArgument(sourceEntry.IsRegularFile,
+      "Internal error: Only regular files can be cloned");
+    var sw = _stopwatchFactory.Create();
+    OnFileCloning(sourceEntry);
+    if (!dryRun) {
+      try {
+        _fileSystem.Extension.CloneFile(sourceEntry, destinationPath);
+      } catch (Exception e) {
+        OnError(sourceEntry.Path, e);
+        return;
+      }
+    }
+    OnFileCloned(sourceEntry, sw.Elapsed, sourceEntry.FileSize);
+  }
+  
+  private void PerformOrScheduleCloneFileIfNeeded(
     FileSystemEntry sourceEntry,
     FileSystemEntry destinationEntry,
     IFileComparer fileComparer,
     bool dryRun,
     List<Task> fileTaskList) {
+    Guard.CheckArgument(sourceEntry.IsRegularFile,
+      "Internal error: Only regular files can be cloned");
 
     // If file size is >= threshold, offload copying to a background task
     if (sourceEntry.FileSize >= _largeFileAsyncThreshold) {
-      fileTaskList.Add(Task.Run(() => PerformFileCloneIfNeeded(sourceEntry, destinationEntry, fileComparer, dryRun)));
+      fileTaskList.Add(Task.Run(() => PerformCloneFileIfNeeded(sourceEntry, destinationEntry, fileComparer, dryRun)));
     }
     else {
-      PerformFileCloneIfNeeded(sourceEntry, destinationEntry, fileComparer, dryRun);
+      PerformCloneFileIfNeeded(sourceEntry, destinationEntry, fileComparer, dryRun);
     }
   }
 
-  private void PerformFileCloneIfNeeded(FileSystemEntry sourceEntry, FileSystemEntry destinationEntry, IFileComparer fileComparer, bool dryRun) {
+  /// <summary>
+  /// Create a clone of <paramref name="sourceEntry"/> as  <paramref name="destinationEntry"/>.
+  ///
+  /// Note: Errors are reported via <see cref="OnError"/> 
+  /// </summary>
+  private void PerformCloneFileIfNeeded(FileSystemEntry sourceEntry, FileSystemEntry destinationEntry, IFileComparer fileComparer, bool dryRun) {
+    Guard.CheckArgument(sourceEntry.IsRegularFile,
+      "Internal error: Only regular files can be cloned");
+
     try {
       bool shouldClone;
       // 1. Decide if we should clone
@@ -666,7 +707,7 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
 
       // 2. Perform clone
       if (shouldClone) {
-        PerformFileClone(sourceEntry, destinationEntry.Path, dryRun);
+        PerformCloneFile(sourceEntry, destinationEntry.Path, dryRun);
       }
     }
     catch (Exception e) {
@@ -674,38 +715,33 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     }
   }
 
-  private void PerformFileClone(FileSystemEntry sourceEntry, FullPath destinationPath, bool dryRun) {
-    var sw = _stopwatchFactory.Create();
-    OnFileCloning(sourceEntry);
-    if (!dryRun) {
-      try {
-        _fileSystem.Extension.CloneFile(sourceEntry, destinationPath);
-      } catch (Exception e) {
-        OnError(sourceEntry.Path, e);
-      }
-    }
-    OnFileCloned(sourceEntry, sw.Elapsed, sourceEntry.FileSize);
-  }
-
-  private bool CompareFiles(IFileComparer fileComparer, FileSystemEntry entry, FileSystemEntry destinationEntry) {
+  /// <summary>
+  /// Compare 2 files <paramref name="sourceEntry"/> and <paramref name="destinationEntry"/>
+  /// using <paramref name="fileComparer"/>. Returns whether the files are considered equal, or <code>false</code>
+  /// if there is an error.
+  ///
+  /// Note: Throws exception if comparison fails unexpectedly 
+  /// </summary>
+  private bool CompareFiles(FileSystemEntry sourceEntry, FileSystemEntry destinationEntry, IFileComparer fileComparer) {
     bool areEqual;
     if (fileComparer.IsFast) {
-      areEqual = fileComparer.CompareFiles(entry, destinationEntry);
-    } else {
-      var sw = _stopwatchFactory.Create();
-      OnFileComparing(entry);
-      var compareData = new CompareFileData(this, sw);
-      areEqual = fileComparer.CompareFiles(entry, destinationEntry, compareData, _compareFileCallback);
-      OnFileCompared(entry, sw.Elapsed, entry.FileSize);
+      areEqual = fileComparer.CompareFiles(sourceEntry, destinationEntry);
     }
-
+    else {
+      var sw = _stopwatchFactory.Create();
+      OnFileComparing(sourceEntry);
+      var compareData = new CompareFileData(this, sw);
+      areEqual = fileComparer.CompareFiles(sourceEntry, destinationEntry, compareData, _compareFileCallback);
+      OnFileCompared(sourceEntry, sw.Elapsed, sourceEntry.FileSize);
+    }
     return areEqual;
   }
-    
 
   /// <summary>
   /// Delete a single file or reparse point if allowed by <paramref name="includeFilter"/>, returning whether
   /// <paramref name="includeFilter"/> returned <code>true</code>. 
+  ///
+  ///  Note: Errors are reported via <see cref="OnError"/> 
   /// </summary>
   private bool DeleteSingleEntry(FileSystemEntry entry, Func<FileSystemEntry, bool> includeFilter) {
     if (!includeFilter(entry)) {
@@ -722,6 +758,20 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     }
     OnEntryDeleted(entry, sw.Elapsed);
     return true;
+  }
+
+  /// <summary>
+  /// Returns the file system entries of <paramref name="directory"/>, or <code>null</code> if there was an error
+  ///
+  ///  Note: Errors are reported via <see cref="OnError"/> 
+  /// </summary>
+  private FromPool<List<FileSystemEntry>>? GetDirectoryEntries(FileSystemEntry directory) {
+    try {
+      return _fileSystem.GetDirectoryFiles(directory.Path);
+    } catch (Exception e) {
+      OnError(directory.Path, e);
+      return null;
+    }
   }
 
   private void OnError(FullPath path, Exception exception) {
@@ -839,19 +889,6 @@ public sealed class ParallelFileSystem : IParallelFileSystem {
     if (handler != null) handler(directoryEntry);
   }
     
-  /// <summary>
-  /// Returns the file system entries of the directory as <paramref name="directoryPath"/>, or <code>null</code>
-  /// if there was an error 
-  /// </summary>
-  private FromPool<List<FileSystemEntry>>? GetDirectoryEntries(FileSystemEntry directory) {
-    try {
-      return _fileSystem.GetDirectoryFiles(directory.Path);
-    } catch (Exception e) {
-      OnError(directory.Path, e);
-      return null;
-    }
-  }
-
   private readonly struct CopyFileData(ParallelFileSystem instance, NoAllocStopwatch stopwatch) {
     public ParallelFileSystem Instance { get; } = instance;
     public NoAllocStopwatch Stopwatch { get; } = stopwatch;
